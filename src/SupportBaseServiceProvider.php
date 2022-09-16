d742d96e9ebe75d05b5dc6625027d57852b0c928
Attia Ahmed <attia.a.ahmed@gmail.com>
2022-09-16 20:22:48 +0300

    add service provider

---
+++ src/SupportBaseServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\ServiceProvider;
+
+class SupportBaseServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
