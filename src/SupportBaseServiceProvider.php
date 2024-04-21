f3526ab27a632d9f88032eb0c70c8fa380631e67
Attia-Ahmed <attia.ahmed@gmail.com>
2024-04-21 22:25:18 +0300

    add testing helpers

--- src/SupportBaseServiceProvider.php
+++ src/SupportBaseServiceProvider.php
@@ -1,14 +1,38 @@
 <?php
 
-use Illuminate\Support\Collection;
+namespace Attia\Support;
+
+use ReflectionClass;
+use ReflectionMethod;
+use Illuminate\Testing\TestResponse;
 use Illuminate\Support\ServiceProvider;
+use Attia\Support\Testing\TestResponseViewMixin;
 
 class SupportBaseServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        if (class_exists(TestResponse::class)) {
+            $this->registerMixin(TestResponse::class, TestResponseViewMixin::class);
+        }
     }
 
+    protected function registerMixin(string $target, $mixin)
+    {
+        $reflectionClass = new ReflectionClass($mixin);
+        $methods = ($reflectionClass)->getMethods(
+            ReflectionMethod::IS_PUBLIC | ReflectionMethod::IS_PROTECTED
+        );
+
+        foreach ($methods as $method) {
+            $target::macro($method->name, function (...$args) use ($mixin, $method) {
+                $mixinClass = new $mixin($this);
+                return [$mixinClass, $method->name](...$args);
+            }
+            );
+        }
+
+    }
     public function boot()
     {
     }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,16 @@
 
 namespace Attia\Support\Tests;
 
+use Illuminate\Foundation\Application;
+use Attia\Support\SupportBaseServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        (new SupportBaseServiceProvider(new Application()))->register();
+    }
 }
